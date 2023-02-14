c8c16a4a9ed696e2e4ef8cd1efb2422d756753fe
Agw-a <lizagwa7@gmail.com>
2023-02-14 08:55:37 +0300

    regexin

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,2 +1,24 @@
-def filter_datum():
-    pass
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    """returns the log message obfuscated:
+
+    Args:
+        field (List[str]): list of strings representing all fields to obfuscate
+        redaction (str): string representing what field will be obfuscated
+        message (str): string representing the log line
+        separator (str): tring representing by which character is
+        separating all fields in the log line (message)
+
+    Returns:
+        str: obfuscated string
+    """
+    for field in fields:
+        message = re.sub(fr'{field}=[^{separator}]*',
+                         f'{field}={redaction}', message)
+    return message
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ## User Data
 <!-- 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 -->
+-- Examples of Personally Identifiable Information (PII)
+-- How to implement a log filter that will obfuscate PII fields
+-- How to encrypt a password and check the validity of an input password
+-- How to authenticate to a database using environment variables
