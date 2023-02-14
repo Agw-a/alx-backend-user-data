c40dc97b9bfac880ba25fb9cb678cbde9cd19c85
Agw-a <lizagwa7@gmail.com>
2023-02-14 09:48:46 +0300

    Encrypt password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''basic password encryption module
+'''
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    '''expects one string argument name password and
+        returns a salted, hashed password, which is a byte string.
+    '''
+    slate = bcrypt.gensalt()
+    pass_bytes = password.encode('utf-8')
+    return bcrypt.hashpw(pass_bytes, slate)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''validate that the provided password matches the hashed password.
+    '''
+    password_bytes = password.encode('utf-8')
+    return bcrypt.checkpw(password_bytes, hashed_password)
