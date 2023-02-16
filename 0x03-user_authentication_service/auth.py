014bdf50bc85f930410459260593567e8333cec5
Agw-a <lizagwa7@gmail.com>
2023-02-16 12:55:48 +0300

    user auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,9 +2,117 @@
 '''Authentication module
 '''
 import bcrypt
+from db import DB
+from user import User
+from uuid import uuid4
+from typing import Union
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     '''returns a slated hash of the input password
     '''
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    '''generate random ID
+    '''
+    return str(uuid4())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        '''Register user
+        '''
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError("User {} already exists".format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        '''checks if login details are valid
+        '''
+        user = None
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                return bcrypt.checkpw(
+                    bcrypt.encode('utf-8'),
+                    user.hashed_password
+                )
+        except NoResultFound:
+            return False
+        return False
+
+    def create_session(self, email: str) -> str:
+        '''New user session
+        '''
+        user = None
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        if user is None:
+            return None
+        sessionId = _generate_uuid()
+        self._db.update_user(user.id, session_id=sessionId)
+        return sessionId
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        '''Get user based on session id
+        '''
+        user = None
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+
+        except NoResultFound:
+            return None
+        return user
+
+    def destroy_session(self, user_id: int) -> None:
+        '''Destroy user session
+        '''
+        if user_id is None:
+            return None
+        self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Gets a password reset token for a user.
+        """
+        user = None
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            user = None
+        if user is None:
+            raise ValueError()
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Updates a user's password given the reset token.
+        """
+        user = None
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            user = None
+        if user is None:
+            raise ValueError()
+        new_password_hash = _hash_password(password)
+        self._db.update_user(
+            user.id,
+            hashed_password=new_password_hash,
+            reset_token=None,
+        )
--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 ## User Data
 <!-- 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 -->
--- Examples of Personally Identifiable Information (PII)
--- How to implement a log filter that will obfuscate PII fields
--- How to encrypt a password and check the validity of an input password
--- How to authenticate to a database using environment variables
+---
+
+Examples of Personally Identifiable Information (PII)
+<br>
+How to implement a log filter that will obfuscate PII fields
+<br>
+How to encrypt a password and check the validity of an input password
+ <br>
+How to authenticate to a database using environment variables
