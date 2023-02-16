80d7757ae049e26248797dde6e3c0f6bcc08662a
Agw-a <lizagwa7@gmail.com>
2023-02-16 08:51:46 +0300

    user and database ops

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+'''DB module
+'''
+
+from sqlalchemy import create_engine, tuple_
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.session import Session
+from user import Base, User
+from sqlalchemy.orm.exc import NoResultFound
+# import os
+
+# BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+# connection_string = 'sqlite:///'+os.path.join(BASE_DIR, 'site.db')
+
+
+class DB():
+    '''DB Class
+    '''
+    def __init__(self) -> None:
+        '''initialize a new DB engine
+        '''
+        # self._engine = create_engine(connection_string, echo=False)
+        self._engine = create_engine("sqlite://a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _sesion(self) -> Session:
+        '''Memoized session object
+        '''
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        '''Add and commit a user
+        '''
+        try:
+            newUser = User(email=email, hashed_password=hashed_password)
+            self.__session.add(newUser)
+            self._sesion.commit(newUser)
+        except Exception:
+            self._sesion.rollback()
+            newUser = None
+        return newUser
+
+    def find_user_by(self, **kwargs) -> User:
+        '''Returns first row found in users filtered by arguments
+        '''
+        fields, values = [], []
+        for key, val in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(val)
+            else:
+                raise InvalidRequestError()
+        res = self.__session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if res is None:
+            raise NoResultFound()
+        return res
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        '''locate a user based on id and update and commit to DB
+        '''
+        user = self.find_user_by(id=user_id)
+        if user is None:
+            return
+        updated = {}
+        for key, val in kwargs.items():
+            if hasattr(User, key):
+                updated[getattr(User, key)] = val
+            else:
+                raise ValueError()
+        self._sesion.query(User).filter(User.id == user_id).update(
+            updated,
+            synchronize_session=False
+        )
+        self.__session.commit()
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+
 '''Delare a User model
 '''
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,7 +11,7 @@ class User(Base):
     '''sqlalchemy model for a users table
     '''
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     email = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
     session_id = Column(String, nullable=True)
